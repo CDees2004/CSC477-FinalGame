348883109e37e9b4fa86b00e0ec662f744227a37
CDees <bcd021@latech.edu>
2026-05-11 11:22:42 -0500

    Give player attack functionality - Establish basic combat

---
+++ Assets/Scripts/Entities/PlayerAttack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Entities
+{
+    public class PlayerAttack : MonoBehaviour
+    {
+        // Need to grab reference to take methods from
+        private Player playerScript;
+
+        private void Start()
+        {
+            playerScript = GetComponentInParent<Player>();
+        }
+
+        // Wrapper method for Deal Damage
+        public void TriggerAttackDamage()
+        {
+            if (playerScript != null) playerScript.DealDamage();
+        }
+    }
+}
